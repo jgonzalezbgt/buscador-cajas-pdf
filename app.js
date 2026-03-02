888deb49ad7d257e49a536b87bece4728df5c496
BGTRUCKING <jgonzalez@bgttrucking.com>
2026-03-02 15:02:08 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // CONFIGURACIÓN CON LA NUEVA URL
-const API_URL = 'https://script.google.com/macros/s/AKfycbz5sr6GgBV8Eqvj8JSLkb7Fo3RNI1gEpCJVrhZ455uaHeSJz3JWDrG83c2mYo05IXwN3Q';
+const API_URL = 'https://script.google.com/macros/s/AKfycbx0_C1Y1RZ0gYc9e7AO6kUAW-ebQreJGYEghZFKt_EYuH5QucjwZU8Y74109xvnakfDWw/exec';
 
 async function buscarPDF() {
     const numeroCaja = document.getElementById('numeroCaja').value.trim();
